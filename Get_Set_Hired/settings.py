dc06cf93e10774241bd7952025a55b34c3eff5f9
lazyXghost <rjtbnslrealme@gmail.com>
2022-10-29 23:27:32 +0530

    Resolved some bugs

--- Get_Set_Hired/settings.py
+++ Get_Set_Hired/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'crispy_forms',
     # ************ SOCIAL_OAUTH *************
     'django.contrib.sites',
     'allauth',
